9be4f2ef933be2152a623d66b4c56119a2ba34fe
QuocThinh73 <ttqthinh2004@gmail.com>
2025-08-30 20:25:55 +0700

    fix: add as_form for BaseSearchRequest

--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
+from fastapi import Form
 from typing import Optional
-from PIL import Image
 
 
 class BaseSearchRequest(BaseModel):
@@ -21,6 +21,41 @@ class BaseSearchRequest(BaseModel):
 
     top_k: int = 100
     
+    @classmethod
+    def as_form(
+        cls,
+        use_embedding_text: bool = Form(False),
+        use_embedding_image: bool = Form(False),
+        use_captioning: bool = Form(False),
+        use_ocr: bool = Form(False),
+        use_object_detection: bool = Form(False),
+        use_translation: bool = Form(False),
+
+        embedding_text: Optional[str] = Form(None),
+        captioning_text: Optional[str] = Form(None),
+        ocr_text: Optional[str] = Form(None),
+        object_detection_text: Optional[str] = Form(None),
+
+        include_batch_ids: Optional[str] = Form(None),
+        exclude_batch_ids: Optional[str] = Form(None),
+
+        top_k: int = Form(100),
+    ) -> "BaseSearchRequest":
+        return cls(
+            use_embedding_text=use_embedding_text,
+            use_embedding_image=use_embedding_image,
+            use_captioning=use_captioning,
+            use_ocr=use_ocr,
+            use_object_detection=use_object_detection,
+            use_translation=use_translation,
+            embedding_text=embedding_text,
+            captioning_text=captioning_text,
+            ocr_text=ocr_text,
+            object_detection_text=object_detection_text,
+            include_batch_ids=include_batch_ids,
+            exclude_batch_ids=exclude_batch_ids,
+            top_k=top_k,
+        )
 
 class TemporalSearchRequest(BaseModel):
     pass
